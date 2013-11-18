0adb39fa9c64f11da387f13abf10df933cd1f170
Naomi Sorbet <naomisorbet@gmail.com>
2013-11-18 14:57:45 -0800

    game working

--- towersOfHanoi.js
+++ towersOfHanoi.js
@@ -1,40 +1,118 @@
+"use strict";
+
 (function (root) {
 	var Hanoi = root.Hanoi = (root.Hanoi || {});
 	var Game = Hanoi.Game = function(){
 		
 		this.stacks = [[3, 2, 1], [], []]
 	}
-		
-	Game.prototype.play(){
-		console.log("How to play message")
-		//loop, maybe with an until won
-		Game.printRods()
-		game.turn
-		game.checkForWin  //unitl?  
-	}	
+	
+	var readline = require('readline');
+	var READER = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+			
 		
 	Game.prototype.checkForWin = function(){
-		if (this.stacks[1] === [3, 2, 1] || this.stacks[2] === [3, 2, 1]){
-			console.log("You win!")
+		function checkTower(tower){
+			if(tower.length === 3) {
+				var subWon = true;
+			}
+			if (tower.length < 1){
+				subWon = false;
+			}
+			else{
+				for (var i = 0; i < tower.length; i++){
+					if (tower[i] != 3 - i){
+						var subWon = false;
+					}
+				}
+			}
+			
+			if (subWon){
+				return true;
+			}
+			else{
+				return false;
+			}
 		}
+		return checkTower(this.stacks[1]) || checkTower(this.stacks[2]);
 	}
 	
 	Game.prototype.printRods = function(){
 		console.log(this.stacks);
 	}
 	
-	Game.prototype.moveDisc = function(src, dest){
-		if(dest.length === 0 ||src[src.length-1] < dest[dest.length-1]){
-			dest.push(src.pop());
-		}
-		else{
-			console.log("not a valid move")
-		}
-		this.checkForWin()
+	Game.prototype.solicitMove = function(){
+		
 	}
+	
+	
+	Game.prototype.isValidMove = function(src, dest){
+		if (this.stacks[src].length === 0){
+			return false;
+		}
+		else if (this.stacks[dest].length === 0){
+
+			return true;
+		}
+		else {
+			return this.stacks[src][this.stacks[src].length-1] < this.stacks[dest][this.stacks[dest].length-1];
+		}
+	}
+	
+	
+	Game.prototype.playTurn = function(){
+		var that = this;
+				this.printRods()
+		READER.question("Name your source tower (0, 1, or 2): ", function(response){
+			var sourceTower = parseInt(response);
+		
+			READER.question("Name your destination tower (0, 1, or 2): ", function(response){
+				var destTower = parseInt(response);
+				if (that.isValidMove(sourceTower, destTower)) {
+					that.moveDisc(sourceTower, destTower)
+				}
+				else {
+					console.log("Invalid move.")
+					that.playTurn();
+				}
+			});
+		
+		});
+	}
+	
+	
+	Game.prototype.moveDisc = function(src, dest){
+		this.stacks[dest].push(this.stacks[src].pop());
+		if (this.checkForWin()) {
+			console.log("You win!")
+			READER.close()
+		}
+		else {
+			this.playTurn()
+		}
+	}
+	
+	Game.prototype.play = function(){
+		console.log("How to play message")
+		this.playTurn()	
+
+	}	
+
 	return Hanoi.Game
 })(this);
 
-myGame = new this.Hanoi.Game;
+var myGame = new this.Hanoi.Game;
 
-myGame.printRods();
+myGame.play();
+
+
+
+
+
+
+
+
+
